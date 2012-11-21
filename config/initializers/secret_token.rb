4ac496b190c03676ec08dab403d8b2414f859ead
kob22 <kob22@debian>
2012-11-21 18:43:58 +0100

    standard app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mag::Application.config.secret_token = 'c5fa0fdc497d038856af8a412e83150d798312c3d520eed39f14955ba7945c72608f39269baa203425a01bf7fb426e62ae770ec6e0df6cc074a64fd7cd18ba94'
